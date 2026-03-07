dc912a2c40be49ccfb0c890303a961059f2e5e34
rauledu7 <rfebrescorderor@gmail.com>
2026-03-07 02:35:39 -0300

    fix: ad faqs

--- components/BrandSlider/BrandSlider.js
+++ components/BrandSlider/BrandSlider.js
@@ -7,7 +7,7 @@ import SliderTour from '../Sliders/SliderTour';
 const BrandSlider = () => {
   return (
     <section id='nuestros-aliados' className={styles.cont}>
-      <h2 className={styles.title}>Nuestros Aliados </h2>
+      <h2 className={styles.title}>Comparamos las Principales Isapres de Chile</h2>
       <SliderTour/>
     </section>
   );
